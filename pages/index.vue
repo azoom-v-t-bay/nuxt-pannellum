e6a081425a2d62bcb9eda5d1e756449a845f343c
Vu Thanh Bay <v.t.bay@azoom.jp>
2020-06-24 14:51:57 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <VuePannellum
+      :hot-spots="hotSpots"
+      src="https://pannellum.org/images/tocopilla.jpg"
+      preview="https://pannellum.org/images/tocopilla-preview.jpg"
+      show-fullscreen
+      auto-rotate
+      compass
+      style="height: 100vh;"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    hotSpots() {
+      return [
+        {
+          pitch: 0,
+          yaw: -30,
+          type: 'info',
+          text: 'Baltimore Museum of Art',
+          URL: 'https://cgworks.jp/',
+        },
+        {
+          pitch: -0.4,
+          yaw: 282.6,
+          type: 'info',
+          text: 'Art Museum Drive',
+        },
+        {
+          pitch: -0.9,
+          yaw: 124.4,
+          type: 'info',
+          text: 'North Charles Street',
+        },
+      ]
+    },
+  },
+}
+</script>
+
+<style></style>
